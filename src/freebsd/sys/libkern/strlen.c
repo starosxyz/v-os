c9ac383d0570debb8940513ce991be85177c8c81
xukangkang1999 <1452111461@qq.com>
2022-04-22 14:08:33 +0800

    4.22change

--- src/freebsd/sys/libkern/strlen.c
+++ src/freebsd/sys/libkern/strlen.c
@@ -80,7 +80,7 @@ static const unsigned long long mask80 = 0x8080808080808080;
 	} while (0)
 
 size_t
-(strlen)(const char *str)
+(vos_strlen)(const char *str)
 {
 	const char *p;
 	const unsigned long long *lp;
